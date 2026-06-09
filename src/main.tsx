b99d100bd343f77bd4209deae1298e36e0e7b421
MSS-Shadow <portadormato@gmail.com>
2026-06-09 00:58:57 -0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,16 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import { registerSW } from "virtual:pwa-register";
+
+registerSW({
+  immediate: true,
+  onNeedRefresh() {
+    console.log("Nueva versión disponible, actualizando...");
+  },
+  onOfflineReady() {
+    console.log("App lista para funcionar offline");
+  },
+});
 
 createRoot(document.getElementById("root")!).render(<App />);
